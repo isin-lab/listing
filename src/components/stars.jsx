442ab88adf917d2445a3ce6a8c5e0319803711d6
ISIN <volkov13057@ya.ru>
2023-12-02 13:35:50 +0300

    commit

--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -14,16 +14,15 @@ const Listing = (props) => {
 
 const Card = (props) => {
   const { item } = props;
-  console.log(item);
   return (
     <div className='item' key={item.listing_id}>
       <div className='item-image'>
         <a href={item.url}>
-          <img src={item.MainImage.url_570xN} alt={item.alt} />
+          <img src={item.MainImage?.url_570xN} alt={item.alt} />
         </a>
       </div>
       <div className='item-details'>
-        <p className='item-title'>{item.title}</p>
+        <p className='item-title'>{item?.title}</p>
         <RenderCode item={item} />
         <RenderLevel item={item} />
       </div>
@@ -33,9 +32,9 @@ const Card = (props) => {
 
 function RenderCode(props) {
   const { item } = props;
-  if (item.currency_code === 'USD') {
+  if (item?.currency_code === 'USD') {
     return (<p className='item-price'>$ {item.price}</p>);
-  } else if (item.currency_code === 'EUR') {
+  } else if (item?.currency_code === 'EUR') {
     return (<p className='item-price'>€ {item.price}</p>);
   } else {
     return (<p className='item-price'>{item.price} {item.currency_code}</p>);
@@ -44,12 +43,12 @@ function RenderCode(props) {
 
 function RenderLevel (props) {
   const { item } = props;
-  if (props > 20) {
-    return (<p class="item-quantity level-high">{item.quantity}</p>)
-  } else if (props <= 20) {
-    return (<p class="item-quantity level-medium">{item.quantity}</p>)
-  } else if (props <= 10) {
-    return (<p class="item-quantity level-low">{item.quantity}</p>)
+  if (item?.quantity > 20) {
+    return (<p className="item-quantity level-high">{item.quantity}</p>)
+  } else if (item?.quantity <= 20) {
+    return (<p className="item-quantity level-medium">{item.quantity}</p>)
+  } else if (item?.quantity <= 10) {
+    return (<p className="item-quantity level-low">{item.quantity}</p>)
   } 
 }
 
